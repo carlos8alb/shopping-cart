9c7cef6d96f178120b757fe91a7ffafd74c72689
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-10-10 19:10:37 -0300

    feat: add services

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Product } from '../models/models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  private URL = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=50';
+  private http = inject(HttpClient);
+
   constructor() { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.URL);
+  }
+
 }
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, share } from 'rxjs';
+import { Product } from '../models/models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingCartService {
+  private shoppingCart$: BehaviorSubject<any> = new BehaviorSubject([]);
+  private products: Product[] = [];
 
-  constructor() { }
+  constructor() {}
+
+  get getProductsShoppingCart() {
+    return this.shoppingCart$.asObservable();
+  }
+
+  addProductShoppingCart(product: Product) {
+    this.products.push(product);
+    this.shoppingCart$.next(this.products);
+  }
+
+  deleteProductShoppingCart(index: number) {
+    this.products.splice(index, 1);
+    this.shoppingCart$.next(this.products);
+  }
+
+  get getTotalShoppingCart() {
+    return this.shoppingCart$.pipe(
+      map((products) => {
+        return products.reduce((acc: number, product: Product) => {
+          return acc + product.price;
+        }, 0);
+      })
+    );
+  }
+
+  get getTotalAmountShoppingCart() {
+    return this.shoppingCart$.pipe(
+      map((products) => products.length)
+    );
+  }
 }
